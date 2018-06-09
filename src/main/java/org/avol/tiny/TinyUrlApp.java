0d36664fbf4adaa4f99e90a270f7797aa904f5f8
Lovababu <lovababu.it@gmail.com>
2018-06-10 00:24:28 +0530

    initial commit

---
+++ src/main/java/org/avol/tiny/TinyUrlApp.java
@@ -0,0 +1,7 @@
+package org.avol.tiny;
+
+/**
+ * Created by lovababu on 09/06/18.
+ */
+public class TinyUrlApp {
+}
---
+++ src/main/java/org/avol/tiny/user/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created by lovababu on 09/06/18.
+ *
+ * Domain Driver Architecture. Logically grouping of each module separately.
+ * It helps you to separate each module as individual deployable unit with minimal changes in future
+ *
+ * User module related stuff goes here.
+ */
+package org.avol.tiny.user;
